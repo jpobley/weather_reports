bc2064784bbf354091ca80c65ccf0f4663627e2e
JP Obley <jpobley@gmail.com>
2020-08-30 22:34:18 -0700

    update build script

--- build.sh
+++ build.sh
@@ -4,19 +4,21 @@ set -e
 
 build_dir=./src
 
-if [ ! -d .venv ]
+if [[ -n "${USE_VENV}" ]]
 then
-    echo 'Setting up virtual environment...'
-    python3 -m venv .venv
+    if [ ! -d .venv ]
+    then
+        echo 'Setting up virtual environment...'
+        python3 -m venv .venv
+    fi
+    source .venv/bin/activate
 fi
 
-source .venv/bin/activate
-
 echo 'Installing requirements.txt...'
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo 'Building protbufs...'
-python -m grpc_tools.protoc \
+python3 -m grpc_tools.protoc \
     -I ./proto \
     --python_out=$build_dir \
     --grpc_python_out=$build_dir \
@@ -25,7 +27,7 @@ python -m grpc_tools.protoc \
 pushd $build_dir
 
 echo 'Installing weather_reports module...'
-python setup.py install
+python3 setup.py install
 
 popd
 
--- src/weather_reports/client.py
+++ src/weather_reports/client.py
@@ -4,25 +4,29 @@ import importlib
 import os
 import sys
 
-CLIENT_TYPE = os.getenv('CLIENT_TYPE')
 
-module = None
-class_name = None
+def run():
+    CLIENT_TYPE = os.getenv('CLIENT_TYPE')
 
-if not CLIENT_TYPE:
-    sys.exit(1)
+    module = None
+    class_name = None
 
-if CLIENT_TYPE == 'sense_hat':
-    module = 'sense_hat_client'
-    class_name = 'SenseHatClient'
-elif CLIENT_TYPE == 'enviro_plus':
-    module = 'enviro_plus_client'
-    class_name = 'EnviroPlusClient'
+    if not CLIENT_TYPE:
+        return
 
-client_module = importlib.import_module(f'weather_reports.device_clients.{module}')
-client_class = getattr(client_module, client_class)
+    if CLIENT_TYPE == 'sense_hat':
+        module = 'sense_hat_client'
+        class_name = 'SenseHatClient'
+    elif CLIENT_TYPE == 'enviro_plus':
+        module = 'enviro_plus_client'
+        class_name = 'EnviroPlusClient'
 
+    client_module = importlib.import_module(f'weather_reports.device_clients.{module}')
+    client_class = getattr(client_module, client_class)
 
-client = client_class()
+    client = client_class()
 
-print(client.report)
+    print(client.report)
+
+if __name__ == '__main__':
+    run()
